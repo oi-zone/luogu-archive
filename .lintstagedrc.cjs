6d01ad4bac39f917e240529c1eaaa16ac1fd61e1
Xinhe Wang <wangxinhe06@gmail.com>
2025-10-30 04:04:23 +0000

    refactor(db): split the schema into multiple files

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 const eslint = "eslint --flag unstable_config_lookup_from_file --fix";
 const prettier = "prettier --write";
-const prisma = "pnpm --filter db exec prisma format --schema";
+const prisma = "pnpm --filter db exec prisma format";
 
 module.exports = {
   "*.{js,mjs,cjs,jsx,ts,tsx}": [eslint, prettier],
