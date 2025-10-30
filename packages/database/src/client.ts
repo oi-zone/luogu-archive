022a22f08dcadbed73120e948d678fb6c9596be6
Xinhe Wang <wangxinhe06@gmail.com>
2025-10-30 04:22:09 +0000

    chore(db): use the new `prisma-client` generator
    https://www.prisma.io/blog/why-prisma-orm-generates-code-into-node-modules-and-why-it-ll-change

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
