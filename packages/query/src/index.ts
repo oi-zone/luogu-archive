36c402fc413b61af1029f2335babae675af37350
Xinhe Wang <wangxinhe06@gmail.com>
2025-12-03 04:49:37 +0000

    feat(web): remove discussion and article lists

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -1,7 +1,4 @@
 export {
-  HOT_DISCUSSION_DEFAULT_LIMIT,
-  HOT_DISCUSSION_DEFAULT_WINDOW_MS,
-  getHotDiscussions,
   getPostWithSnapshot,
   getPostRepliesWithLatestSnapshot,
   getPostUserReplyInference,
@@ -10,20 +7,13 @@ export {
   getPostBasicInfo,
   getPostSnapshotsTimeline,
 } from "./discussion.js";
-export type { HotDiscussionSummary } from "./discussion.js";
 export {
-  ARTICLE_SCORE_WEIGHT,
-  FEATURED_ARTICLE_DEFAULT_LIMIT,
-  FEATURED_ARTICLE_DEFAULT_WINDOW_MS,
-  FEATURED_ARTICLE_DEFAULT_UPVOTE_DECAY_MS,
-  getFeaturedArticles,
   getArticleWithSnapshot,
   getArticleBasicInfo,
   getArticleComments,
   getArticleComment,
   getArticleSnapshotsTimeline,
 } from "./article.js";
-export type { FeaturedArticleSummary } from "./article.js";
 export { getUserWithLatestSnapshot } from "./user.js";
 export {
   getUserProfileBundle,
